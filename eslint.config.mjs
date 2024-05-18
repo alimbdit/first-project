e12257fb49044898144de28485b7965b0004af39
alimbdit <alimbdit@gmail.com>
2024-05-18 20:28:19 +0600

    modules added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,44 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
 export default [
+  js.configs.recommended,
   {
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ],
+    files: ['*.ts', '*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+    },
+  },
+  {
+    // extends: [
+    //   'eslint:recommended',
+    //   'plugin:@typescript-eslint/recommended',
+    //   'prettier',
+    // ],
+    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
     ignores: ['**/node_modules/', '.dist/'],
     languageOptions: {
       globals: {
         ...globals.browser,
         process: 'readonly',
       },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
     },
     rules: {
       'no-unused-vars': 'error',
